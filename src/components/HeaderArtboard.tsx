ca1f3bff9ff89041eda6068da00666d8ed0b66c3
Diogo Machado <diogo@diogomachado.com>
2022-04-28 20:49:43 -0300

    Added Navigator

--- src/components/HeaderArtboard.tsx
+++ src/components/HeaderArtboard.tsx
@@ -4,31 +4,37 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function HeaderArtboard({ title }: { title: string }) {
   return (
-    <div className="header" data-testid="header">
-      <Link to="/" className="close-button" data-testid="close-button">
-        <img data-testid="close-image" src="/assets/close.svg" alt="Close" />
-      </Link>
-      <img
-        className="separator"
-        data-testid="separator"
-        src="/assets/separator.svg"
-        alt="Separator"
-      />
-      <div className="artboard-navigation" data-testid="artboard-navigation">
-        <button>
-          <img
-            data-testid="artboard-navigation-left"
-            src="/assets/arrow-left.svg"
-          />
-        </button>
-        <button>
-          <img
-            data-testid="artboard-navigation-right"
-            src="/assets/arrow-right.svg"
-          />
-        </button>
+    <div className="header-artboard" data-testid="header">
+      <div className="left">
+        <Link to="/" className="close-button" data-testid="close-button">
+          <img data-testid="close-image" src="/assets/close.svg" alt="Close" />
+        </Link>
+        <img
+          className="separator"
+          data-testid="separator"
+          src="/assets/separator.svg"
+          alt="Separator"
+        />
+        <div className="artboard-navigation" data-testid="artboard-navigation">
+          <button>
+            <img
+              data-testid="artboard-navigation-left"
+              src="/assets/arrow-left.svg"
+            />
+          </button>
+          <span>4</span>
+          <img src="/assets/breadcrumb.svg" alt="Breadcrumb" />
+          <span>8</span>
+          <button>
+            <img
+              data-testid="artboard-navigation-right"
+              src="/assets/arrow-right.svg"
+            />
+          </button>
+        </div>
       </div>
-      {title}
+      <div className="center">{title}</div>
+      <div className="right"></div>
     </div>
   );
 }
